4867b4055095ae7c9a1cf4f6085a399815c1b7d8
thiagoewdev Thiago <tigoew@gmail.com>
2024-04-29 15:04:45 +0000

    0.0.2

--- scripts.js
+++ scripts.js
@@ -4,10 +4,11 @@ var abrirFecharMenuBtn = document.getElementById("abrir-fechar-menu");
 abrirFecharMenuBtn.addEventListener("click", function () {
   if (menuLateral.style.left === "-220px") {
     menuLateral.style.left = "0";
-    abrirFecharMenuBtn.style.left = "0px"; /* Ajuste da posição */
+    document.getElementById("conteudo").style.marginLeft =
+      "240px"; /* Ajuste para evitar sobreposição */
   } else {
     menuLateral.style.left = "-220px";
-    abrirFecharMenuBtn.style.left = "0px"; /* Ajuste da posição */
+    document.getElementById("conteudo").style.marginLeft =
+      "20px"; /* Ajuste para evitar sobreposição */
   }
 });
-
